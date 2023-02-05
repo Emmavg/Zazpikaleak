2c5fd6c1a4cf45fa22b382332ef48e4d42fe8de6
EmmaVélez <emmavelgarci@hotmail.com>
2023-02-05 22:13:56 +0100

    Alumnos mas o menos

--- DidaktikApp_Zazpikaleak/app/src/main/java/com/example/didaktikapp_zazpikaleak/MainAlumnos.java
+++ DidaktikApp_Zazpikaleak/app/src/main/java/com/example/didaktikapp_zazpikaleak/MainAlumnos.java
@@ -3,6 +3,7 @@ package com.example.didaktikapp_zazpikaleak;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -13,17 +14,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
-public class MainAlumnos extends AppCompatActivity {
+public class MainAlumnos extends AppCompatActivity implements MainActivity_Dialogo.OnDialogoConfirmacionListener {
 
     private ListView vista;
     private ArrayList<String> alum;
-    private ArrayList<Alumno> arrLAlumnos;
+    private FloatingActionButton addAlum;
+    private MainAlumnosDialogo dialogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +36,26 @@ public class MainAlumnos extends AppCompatActivity {
         setContentView(R.layout.activity_main_alumnos);
 
         vista = (ListView) findViewById(R.id.vista);
+        addAlum = findViewById(R.id.btnAddAlumnos);
 
         ZazpiKaleakSQLiteHelper zazpidbh = new ZazpiKaleakSQLiteHelper(getBaseContext(), "ZazpikaleakDB", null, 1);
         AlumnoDao ad = new AlumnoDao();
-        //ad.insertarAlumno(zazpidbh, "Emma");
+
+
+        addAlum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                dialogo = new MainAlumnosDialogo();
+                dialogo.show(fragmentManager, "Añadir Alumno");
+            }
+        });
 
         alum = ad.cogerAlumnos(zazpidbh);
 
-        for (String s : alum){
-            System.out.println(s+"**********************************************");
-            arrLAlumnos.add(new Alumno(s));
-        }
-
-        for (Alumno s : arrLAlumnos){
-            System.out.println(s.getNombre()+"**********************************************");
-        }
-
-
         //Adaptador
         AdaptadorAlumnos adapt =
-                new AdaptadorAlumnos(this, arrLAlumnos);
+                new AdaptadorAlumnos(this, alum);
         vista.setAdapter(adapt);
 
         //Eventos
@@ -60,14 +65,24 @@ public class MainAlumnos extends AppCompatActivity {
                                     int position, long id) {
                 //Seleccion del alumno:
                 String opcionSeleccionada =
-                        ((Alumno)parent.getItemAtPosition(position)).getNombre();
-                long opcion = parent.getItemIdAtPosition(position);
+                        (String) parent.getItemAtPosition(position);
+                //long opcion = parent.getItemIdAtPosition(position);
             }
         });
     }
 
-    class AdaptadorAlumnos extends ArrayAdapter<Alumno> {
-        public AdaptadorAlumnos(@NonNull Context context, ArrayList<Alumno> a) {
+    @Override
+    public void onPossitiveButtonClick() {
+        String nombre = ((EditText) dialogo.getDialog().findViewById(R.id.editTextJugador)).getText().toString();
+        if (!nombre.isEmpty()){
+            ZazpiKaleakSQLiteHelper zazpidbh = new ZazpiKaleakSQLiteHelper(getBaseContext(), "ZazpikaleakDB", null, 1);
+            AlumnoDao ad = new AlumnoDao();
+            ad.insertarAlumno(zazpidbh, nombre);
+        }
+    }
+
+    class AdaptadorAlumnos extends ArrayAdapter<String> {
+        public AdaptadorAlumnos(@NonNull Context context, ArrayList<String> a) {
             super(context, R.layout.listitem_alumnos, a);
         }
         @NonNull
@@ -77,7 +92,7 @@ public class MainAlumnos extends AppCompatActivity {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             View item = inflater.inflate(R.layout.listitem_alumnos, null);
             TextView nombre = (TextView)item.findViewById(R.id.nombre);
-            nombre.setText(arrLAlumnos.get(position).getNombre());
+            nombre.setText(alum.get(position));
             return (item);
         }
     }
---
+++ DidaktikApp_Zazpikaleak/app/src/main/java/com/example/didaktikapp_zazpikaleak/MainAlumnosDialogo.java
@@ -0,0 +1,64 @@
+package com.example.didaktikapp_zazpikaleak;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class MainAlumnosDialogo extends DialogFragment {
+
+    private OnDialogoConfirmacionListener listener;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        setCancelable(false);
+        builder.setView(inflater.inflate(R.layout.activity_main_alumnos_dialogo, null))
+
+                .setPositiveButton("Aceptar",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                listener.onPossitiveButtonClick();
+                            }
+                        })
+                .setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+
+        return builder.create();
+    }
+
+    public interface OnDialogoConfirmacionListener{
+        void onPossitiveButtonClick(); //Eventos Botón Positivos
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            listener = (OnDialogoConfirmacionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    " no Implemento OnDialogoConfirmacionListener");
+        }
+    }
+}
+
+
